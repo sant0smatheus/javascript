ce0eb6ea62d9f58ca3d737ff271029d9b8ac8196
Matheus Henrique <matheus.fsrj@hotmail.com>
2025-09-17 20:56:00 +0100

    aula13

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,26 @@
+// Teste no final
+var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 6)
+
+/* Teste no início
+var contador = 1
+while (contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador++ // c = c + 1
+}
+*/
+
+/* 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') 
+*/
+
+
+

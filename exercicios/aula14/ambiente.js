929b91fecf1570c39f1542abb6d77388b4c9ec8f
Matheus Henrique <matheus.fsrj@hotmail.com>
2025-09-17 21:35:19 +0100

    aula14

---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,11 @@
+for (contador = 1; contador <= 10; contador++) {
+    console.log(`Passo ${contador}`)
+}
+
+/*
+var contador = 1
+while (contador <= 10) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+*/

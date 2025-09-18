55f14d7e196110102d819f7fae7e64b843999336
Matheus Henrique <matheus.fsrj@hotmail.com>
2025-09-18 21:01:48 +0100

    Update script.js

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -1,9 +1,19 @@
-var inicio = document.getElementById(Number('txtin'))
-var fim = document.getElementById(Number('txtfim'))
-var passo = document.getElementById(Number('txtpas'))
-
 function contar() {
-for (inicio = 0; inicio <= fim; inicio = inicio + passo) {
-    console.log(`${inicio}`)
-}
+    let inicio = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+       res.innerHTML = 'Contando: '
+       let i = Number(inicio.value)
+       let f = Number(fim.value)
+       let p = Number(passo.value)
+
+       for(let c = inicio; c <= fim; c += passo) {
+            res.innerHTML += `${c} `
+       }
+    }
 }

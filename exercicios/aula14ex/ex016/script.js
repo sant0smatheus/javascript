d99261f9f2dffe5ab48bca2d29e3d2ed90b7c076
Matheus Henrique <matheus.fsrj@hotmail.com>
2025-09-20 10:42:49 +0100

    ex016

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -5,15 +5,28 @@ function contar() {
     let res = document.getElementById('res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        //window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
     } else {
-       res.innerHTML = 'Contando: '
+       res.innerHTML = 'Contando: <br>'
        let i = Number(inicio.value)
        let f = Number(fim.value)
        let p = Number(passo.value)
-
-       for(let c = inicio; c <= fim; c += passo) {
-            res.innerHTML += `${c} `
+       if (p <= 0) {
+        window.alert('Passo inválido! Considerando PASSO 1')
+        p = 1
        }
+       if (i < f) {
+            // contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+       } else {
+            // contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+       }
+       res.innerHTML += `\u{1F3C1}`
     }
 }
